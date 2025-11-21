17963e1161b3d3ff13c192db30f0996ce4a839cc
Prashant S-Kolekar <prashant.s-kolekar@nagarro.com>
2025-11-21 19:56:42 +0530

    change ip

--- UI/static/js/chat.js
+++ UI/static/js/chat.js
@@ -4,7 +4,7 @@
         const reportProgress = document.getElementById("reportProgress");
         let sessionId = sessionStorage.getItem('session_id');
         
-        const BASE_URL = "http://3.110.196.241:8000";
+        const BASE_URL = "http://3.109.121.222:8000";
         
         function generateUUID() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
